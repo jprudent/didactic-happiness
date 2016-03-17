00d1b39c8d2efc223fbb4bffce9a6e01208e7e76
Jérôme Prudent <jprudent@gmail.com>
2016-03-17 14:19:52 +0100

    [MY-DEBUGGER] ptrace step 1 ok

---
+++ my-debugger/ptrace_ex1.c
@@ -0,0 +1,27 @@
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/user.h>
+#include <sys/reg.h>
+
+int main()
+{   pid_t child;
+    long orig_rax;
+    child = fork();
+    if(child == 0) {
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        execl("toto", "toto", NULL);
+    }
+    else {
+        wait(NULL);
+        orig_rax = ptrace(PTRACE_PEEKUSER,
+                          child, 8 * ORIG_RAX,
+                          NULL);
+        printf("The child made a "
+               "system call %ld\n", orig_rax);
+        ptrace(PTRACE_CONT, child, NULL, NULL);
+    }
+    return 0;
+}
