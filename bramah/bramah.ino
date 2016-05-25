551f6b2e1a3f0e18bfc3510084400d6f21ce3b14
Jérôme Prudent <jprudent@gmail.com>
2016-05-26 00:21:45 +0200

    [BRAMAH] Clean the code

--- bramah/bramah.ino
+++ bramah/bramah.ino
@@ -1,32 +1,45 @@
-/* Arduino USB HID Keyboard Demo
- * Random Key/Random Delay
- */
+#include "SHA256.h"
+#include "Base64.h"
 
-uint8_t buf[8] = { 
-  0 };   /* Keyboard report buffer */
+const char* PASSWORD = "pass";
+const char* SITE = "gmail.com";
+const size_t HLEN = 32;
 
-void setup() 
-{
+SHA256 hashcode;
+
+// Compute the hash of s1 + s2
+// output: the output string of size HLEN
+void hash(char * output, const char * s1, const char * s2) {
+  hashcode.reset();
+  hashcode.update(s1, strlen(s1));
+  hashcode.update(s2, strlen(s2));
+  hashcode.finalize(output, HLEN);
+}
+
+// Encode input in ASCII readable output
+// output must have HLEN size
+// output may not be reversable to input
+void encode_to_ascii(char * output, char * input) {
+  const int b64l = base64_enc_len(HLEN);
+  char b64encoded[b64l];
+  base64_encode(b64encoded, input, HLEN);
+  memcpy(output, input, HLEN);
+}
+
+// output: the output string of size HLEN
+void generate_password(char * output, const char * master_password, const char * memo) {
+  char sha[HLEN];
+  hash(sha, master_password, memo);
+  encode_to_ascii(output, sha);
+}
+
+void setup() {
   Serial.begin(9600);
-  randomSeed(analogRead(0));
-  delay(200);
+
+  char output[HLEN];
+  generate_password(output, PASSWORD, SITE);
 }
 
-void loop() 
-{
-  int randomChar = random(4, 130);
-  long randomDelay = random(1000, 10000);
+void loop() {
 
-  delay(randomDelay);
-
-  buf[2] = randomChar;    // Random character
-  Serial.write(buf, 8); // Send keypress
-  releaseKey();
-}
-
-void releaseKey() 
-{
-  buf[0] = 0;
-  buf[2] = 0;
-  Serial.write(buf, 8); // Release key  
 }
