d33908f1509536c04ffb2aadcc85bf83d6ac61f2
Jérôme Prudent <jprudent@gmail.com>
2016-09-14 05:07:16 +0200

    [CRYPTOPALS] Set 1.1

---
+++ cryptopals/src/cryptopals/ascii_bytes.clj
@@ -0,0 +1,47 @@
+(ns cryptopals.ascii-bytes
+  "This namespaces is about bytes written as ascii")
+
+(def ^:static hexchar->byte
+  {\0 0x0, \1 0x1, \2 0x2, \3 0x3, \4 0x4, \5 0x5, \6 0x6, \7 0x7, \8 0x8, \9 0X9,
+   \A 0xA, \B 0xB, \C 0xC, \D 0xD, \E 0xE, \F 0xF
+   \a 0xA, \b 0xB, \c 0xC, \d 0xD, \e 0xE, \f 0xF})
+
+(defn hexstring->bytes
+  "Take an hexadecimal string and returns a seq of bytes"
+  [s]
+  (map (fn [[b1 b0]]
+         (bit-or (bit-shift-left (hexchar->byte b1) 4)
+                 (hexchar->byte b0)))
+       (partition 2 s)))
+
+(def ^:static base64-index-table
+  {0  \A, 16 \Q, 32 \g, 48 \w
+   1  \B, 17 \R, 33 \h, 49 \x
+   2  \C, 18 \S, 34 \i, 50 \y
+   3  \D, 19 \T, 35 \j, 51 \z
+   4  \E, 20 \U, 36 \k, 52 \0
+   5  \F, 21 \V, 37 \l, 53 \1
+   6  \G, 22 \W, 38 \m, 54 \2
+   7  \H, 23 \X, 39 \n, 55 \3
+   8  \I, 24 \Y, 40 \o, 56 \4
+   9  \J, 25 \Z, 41 \p, 57 \5
+   10 \K, 26 \a, 42 \q, 58 \6
+   11 \L, 27 \b, 43 \r, 59 \7
+   12 \M, 28 \c, 44 \s, 60 \8
+   13 \N, 29 \d, 45 \t, 61 \9
+   14 \O, 30 \e, 46 \u, 62 \+
+   15 \P, 31 \f, 47 \v, 63 \/})
+
+(defn bytes->base64
+  [bytes]
+  (->> (map
+         (fn [[b0 b1 b2]]
+           [(bit-shift-right (bit-and 2r11111100 b0) 2)
+            (bit-or (bit-shift-left (bit-and 2r00000011 b0) 4)
+                    (bit-shift-right (bit-and 2r11110000 b1) 4))
+            (bit-or (bit-shift-left (bit-and 2r00001111 b1) 2)
+                    (bit-shift-right (bit-and 2r11000000 b2) 6))
+            (bit-and 2r00111111 b2)])
+         (partition 3 bytes))
+       (mapcat #(map base64-index-table %))
+       (apply str)))
--- cryptopals/test/cryptopals/core_test.clj
+++ cryptopals/test/cryptopals/core_test.clj
@@ -1,7 +1,9 @@
 (ns cryptopals.core-test
   (:require [clojure.test :refer :all]
-            [cryptopals.core :refer :all]))
+            [cryptopals.core :refer :all]
+            [cryptopals.ascii-bytes :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest set_1_1
+  (testing "Base 64"
+    (is (= "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+           (bytes->base64 (hexstring->bytes "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))))))
