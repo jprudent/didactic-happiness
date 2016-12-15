a2eaa3bc3ec9542a7a7163eaf42e0b4861ccb6f4
Jérôme Prudent <jprudent@gmail.com>
2016-12-15 21:21:09 +0100

    [BRAMAH] Fixed bug on key expansion

--- bramah/bramust/src/hmac/mod.rs
+++ bramah/bramust/src/hmac/mod.rs
@@ -15,7 +15,7 @@ fn xor(v1: &Vec<u8>, v2: &Vec<u8>) -> Vec<u8> {
 }
 
 pub fn hmac_sha256(msg: Vec<u8>, key: Vec<u8>) -> Vec<u32> {
-    let k:Vec<u8> = if key.len() > 8 {
+    let k:Vec<u8> = if key.len() > 32 {
         sha256::hash(key).iter().flat_map(bytes::u32_to_vec_u8).collect()
     } else {
         concat(&key, &repeat(0_u8).take(64 - key.len()).collect())
@@ -30,4 +30,5 @@ pub fn hmac_sha256(msg: Vec<u8>, key: Vec<u8>) -> Vec<u32> {
 #[test]
 fn should_hmac_any_message() {
     assert_eq!(hmac_sha256(vec![], vec![]), vec![0xb613679a, 0x0814d9ec, 0x772f95d7, 0x78c35fc5, 0xff1697c4, 0x93715653, 0xc6c71214, 0x4292c5ad]);
+    assert_eq!(hmac_sha256(vec![102, 111, 111], vec![0; 16]), vec![0x0c0d98f7, 0xe3d9d45e, 0x72e8877b, 0xc1b10432, 0x7efb9c07, 0xb18f2ffe, 0xced76d81, 0x307f1fff]);
 }
