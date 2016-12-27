6141061ccbe3a3206c12bb7d76379d2dbb53d02b
Jérôme Prudent <jprudent@gmail.com>
2016-12-27 21:12:02 +0100

    [EPICENE] Moar LD

--- epicene/src/cpu/mod.rs
+++ epicene/src/cpu/mod.rs
@@ -90,9 +90,9 @@ fn should_get_value_from_registers() {
 
 pub struct SwitchBasedDecoder {}
 
-struct Load<L, R> {
-    left_operand: L,
-    right_operand: R,
+struct Load<DESTINATION, SOURCE> {
+    destination: DESTINATION,
+    source: SOURCE,
     size: Double,
     cycles: Cycle
 }
@@ -115,8 +115,8 @@ enum MemoryPointerOperand {
 
 impl Opcode for Load<RegisterOperand, ImmediateOperand> {
     fn exec(&self, cpu: &mut ComputerUnit) {
-        let ImmediateOperand(immediate_value) = self.right_operand;
-        match self.left_operand {
+        let ImmediateOperand(immediate_value) = self.source;
+        match self.destination {
             RegisterOperand::A => panic!("This case doesn't exists"),
             RegisterOperand::B => cpu.set_register_b(immediate_value),
             RegisterOperand::C => cpu.set_register_c(immediate_value),
@@ -132,7 +132,7 @@ impl Opcode for Load<RegisterOperand, ImmediateOperand> {
 
 impl Opcode for Load<RegisterOperand, RegisterOperand> {
     fn exec(&self, cpu: &mut ComputerUnit) {
-        let word = match self.right_operand {
+        let word = match self.source {
             RegisterOperand::A => cpu.get_a_register(),
             RegisterOperand::B => cpu.get_b_register(),
             RegisterOperand::C => cpu.get_c_register(),
@@ -141,7 +141,7 @@ impl Opcode for Load<RegisterOperand, RegisterOperand> {
             RegisterOperand::H => cpu.get_h_register(),
             RegisterOperand::L => cpu.get_l_register(),
         };
-        match self.left_operand {
+        match self.destination {
             RegisterOperand::A => cpu.set_register_a(word),
             RegisterOperand::B => cpu.set_register_b(word),
             RegisterOperand::C => cpu.set_register_c(word),
@@ -155,12 +155,35 @@ impl Opcode for Load<RegisterOperand, RegisterOperand> {
     }
 }
 
+impl Opcode for Load<MemoryPointerOperand, RegisterOperand> {
+    fn exec(&self, cpu: &mut ComputerUnit) {
+        let word = match self.source {
+            RegisterOperand::A => cpu.get_a_register(),
+            RegisterOperand::B => cpu.get_b_register(),
+            RegisterOperand::C => cpu.get_c_register(),
+            RegisterOperand::D => cpu.get_d_register(),
+            RegisterOperand::E => cpu.get_e_register(),
+            RegisterOperand::H => cpu.get_h_register(),
+            RegisterOperand::L => cpu.get_l_register(),
+        };
+
+        match self.destination {
+            MemoryPointerOperand::HL => {
+                let address = cpu.get_hl_register();
+                cpu.set_word_at(address, word)
+            }
+        }
+        cpu.inc_pc(self.size);
+        cpu.cycles = cpu.cycles + self.cycles;
+    }
+}
+
 impl Opcode for Load<RegisterOperand, MemoryPointerOperand> {
     fn exec(&self, cpu: &mut ComputerUnit) {
-        let word = match self.right_operand {
+        let word = match self.source {
             MemoryPointerOperand::HL => cpu.word_at(cpu.get_hl_register())
         };
-        match self.left_operand {
+        match self.destination {
             RegisterOperand::A => cpu.set_register_a(word),
             RegisterOperand::B => cpu.set_register_b(word),
             RegisterOperand::C => cpu.set_register_c(word),
@@ -178,102 +201,124 @@ trait Opcode {
     fn exec(&self, cpu: &mut ComputerUnit);
 }
 
+fn between(word: Word, lower_bound: Word, upper_bound: Word) -> bool {
+    word >= lower_bound && word <= upper_bound
+}
+
 impl SwitchBasedDecoder {
     fn decode(&self, word: Word, cpu: &ComputerUnit) -> Box<Opcode> {
-        if (word <= 0x2E) && ((word & 0b111) == 0b110) {
+        if between(word, 0x70, 0x75) {
+            let ld_ptr_hl_r = Load {
+                destination: MemoryPointerOperand::HL,
+                source: RegisterOperand::A,
+                size: 1,
+                cycles: 8
+            };
+
+            Box::new(
+                match word {
+                    0x70 => Load { source: RegisterOperand::B, ..ld_ptr_hl_r },
+                    0x71 => Load { source: RegisterOperand::C, ..ld_ptr_hl_r },
+                    0x72 => Load { source: RegisterOperand::D, ..ld_ptr_hl_r },
+                    0x73 => Load { source: RegisterOperand::E, ..ld_ptr_hl_r },
+                    0x74 => Load { source: RegisterOperand::H, ..ld_ptr_hl_r },
+                    0x75 => Load { source: RegisterOperand::L, ..ld_ptr_hl_r },
+                    _ => panic!(format!("unhandled opcode : 0x{:02X}", word))
+                })
+        } else if (word <= 0x2E) && ((word & 0b111) == 0b110) {
             let ld_r_w = Load {
-                left_operand: RegisterOperand::B,
-                right_operand: ImmediateOperand(cpu.word_at(cpu.get_pc_register() + 1)),
+                destination: RegisterOperand::B,
+                source: ImmediateOperand(cpu.word_at(cpu.get_pc_register() + 1)),
                 size: 2,
                 cycles: 8
             };
 
             Box::new(
                 match word {
-                    0x06 => Load { left_operand: RegisterOperand::B, ..ld_r_w },
-                    0x0E => Load { left_operand: RegisterOperand::C, ..ld_r_w },
-                    0x16 => Load { left_operand: RegisterOperand::D, ..ld_r_w },
-                    0x1E => Load { left_operand: RegisterOperand::E, ..ld_r_w },
-                    0x26 => Load { left_operand: RegisterOperand::H, ..ld_r_w },
-                    0x2E => Load { left_operand: RegisterOperand::L, ..ld_r_w },
+                    0x06 => Load { destination: RegisterOperand::B, ..ld_r_w },
+                    0x0E => Load { destination: RegisterOperand::C, ..ld_r_w },
+                    0x16 => Load { destination: RegisterOperand::D, ..ld_r_w },
+                    0x1E => Load { destination: RegisterOperand::E, ..ld_r_w },
+                    0x26 => Load { destination: RegisterOperand::H, ..ld_r_w },
+                    0x2E => Load { destination: RegisterOperand::L, ..ld_r_w },
                     _ => panic!(format!("unhandled opcode : 0x{:02X}", word))
                 })
         } else if (word >= 0x46) && (word <= 0x7E) && ((word & 0b111) == 0b110) {
             let ld_r_ptr_hl = Load {
-                left_operand: RegisterOperand::A,
-                right_operand: MemoryPointerOperand::HL,
+                destination: RegisterOperand::A,
+                source: MemoryPointerOperand::HL,
                 size: 1,
                 cycles: 8
             };
             Box::new(
                 match word {
-                    0x7E => Load { left_operand: RegisterOperand::A, ..ld_r_ptr_hl },
-                    0x46 => Load { left_operand: RegisterOperand::B, ..ld_r_ptr_hl },
-                    0x4E => Load { left_operand: RegisterOperand::C, ..ld_r_ptr_hl },
-                    0x56 => Load { left_operand: RegisterOperand::D, ..ld_r_ptr_hl },
-                    0x5E => Load { left_operand: RegisterOperand::E, ..ld_r_ptr_hl },
-                    0x66 => Load { left_operand: RegisterOperand::H, ..ld_r_ptr_hl },
-                    0x6E => Load { left_operand: RegisterOperand::L, ..ld_r_ptr_hl },
+                    0x7E => Load { destination: RegisterOperand::A, ..ld_r_ptr_hl },
+                    0x46 => Load { destination: RegisterOperand::B, ..ld_r_ptr_hl },
+                    0x4E => Load { destination: RegisterOperand::C, ..ld_r_ptr_hl },
+                    0x56 => Load { destination: RegisterOperand::D, ..ld_r_ptr_hl },
+                    0x5E => Load { destination: RegisterOperand::E, ..ld_r_ptr_hl },
+                    0x66 => Load { destination: RegisterOperand::H, ..ld_r_ptr_hl },
+                    0x6E => Load { destination: RegisterOperand::L, ..ld_r_ptr_hl },
                     _ => panic!(format!("unhandled opcode : 0x{:02X}", word))
                 })
         } else {
             let ld_r_r = Load {
-                left_operand: RegisterOperand::A,
-                right_operand: RegisterOperand::A,
+                destination: RegisterOperand::A,
+                source: RegisterOperand::A,
                 size: 1,
                 cycles: 4
             };
             Box::new(
                 match word {
-                    0x7F => Load { left_operand: RegisterOperand::A, right_operand: RegisterOperand::A, ..ld_r_r },
-                    0x78 => Load { left_operand: RegisterOperand::A, right_operand: RegisterOperand::B, ..ld_r_r },
-                    0x79 => Load { left_operand: RegisterOperand::A, right_operand: RegisterOperand::C, ..ld_r_r },
-                    0x7A => Load { left_operand: RegisterOperand::A, right_operand: RegisterOperand::D, ..ld_r_r },
-                    0x7B => Load { left_operand: RegisterOperand::A, right_operand: RegisterOperand::E, ..ld_r_r },
-                    0x7C => Load { left_operand: RegisterOperand::A, right_operand: RegisterOperand::H, ..ld_r_r },
-                    0x7D => Load { left_operand: RegisterOperand::A, right_operand: RegisterOperand::L, ..ld_r_r },
+                    0x7F => Load { destination: RegisterOperand::A, source: RegisterOperand::A, ..ld_r_r },
+                    0x78 => Load { destination: RegisterOperand::A, source: RegisterOperand::B, ..ld_r_r },
+                    0x79 => Load { destination: RegisterOperand::A, source: RegisterOperand::C, ..ld_r_r },
+                    0x7A => Load { destination: RegisterOperand::A, source: RegisterOperand::D, ..ld_r_r },
+                    0x7B => Load { destination: RegisterOperand::A, source: RegisterOperand::E, ..ld_r_r },
+                    0x7C => Load { destination: RegisterOperand::A, source: RegisterOperand::H, ..ld_r_r },
+                    0x7D => Load { destination: RegisterOperand::A, source: RegisterOperand::L, ..ld_r_r },
 
-                    0x40 => Load { left_operand: RegisterOperand::B, right_operand: RegisterOperand::B, ..ld_r_r },
-                    0x41 => Load { left_operand: RegisterOperand::B, right_operand: RegisterOperand::C, ..ld_r_r },
-                    0x42 => Load { left_operand: RegisterOperand::B, right_operand: RegisterOperand::D, ..ld_r_r },
-                    0x43 => Load { left_operand: RegisterOperand::B, right_operand: RegisterOperand::E, ..ld_r_r },
-                    0x44 => Load { left_operand: RegisterOperand::B, right_operand: RegisterOperand::H, ..ld_r_r },
-                    0x45 => Load { left_operand: RegisterOperand::B, right_operand: RegisterOperand::L, ..ld_r_r },
+                    0x40 => Load { destination: RegisterOperand::B, source: RegisterOperand::B, ..ld_r_r },
+                    0x41 => Load { destination: RegisterOperand::B, source: RegisterOperand::C, ..ld_r_r },
+                    0x42 => Load { destination: RegisterOperand::B, source: RegisterOperand::D, ..ld_r_r },
+                    0x43 => Load { destination: RegisterOperand::B, source: RegisterOperand::E, ..ld_r_r },
+                    0x44 => Load { destination: RegisterOperand::B, source: RegisterOperand::H, ..ld_r_r },
+                    0x45 => Load { destination: RegisterOperand::B, source: RegisterOperand::L, ..ld_r_r },
 
-                    0x48 => Load { left_operand: RegisterOperand::C, right_operand: RegisterOperand::B, ..ld_r_r },
-                    0x49 => Load { left_operand: RegisterOperand::C, right_operand: RegisterOperand::C, ..ld_r_r },
-                    0x4A => Load { left_operand: RegisterOperand::C, right_operand: RegisterOperand::D, ..ld_r_r },
-                    0x4B => Load { left_operand: RegisterOperand::C, right_operand: RegisterOperand::E, ..ld_r_r },
-                    0x4C => Load { left_operand: RegisterOperand::C, right_operand: RegisterOperand::H, ..ld_r_r },
-                    0x4D => Load { left_operand: RegisterOperand::C, right_operand: RegisterOperand::L, ..ld_r_r },
+                    0x48 => Load { destination: RegisterOperand::C, source: RegisterOperand::B, ..ld_r_r },
+                    0x49 => Load { destination: RegisterOperand::C, source: RegisterOperand::C, ..ld_r_r },
+                    0x4A => Load { destination: RegisterOperand::C, source: RegisterOperand::D, ..ld_r_r },
+                    0x4B => Load { destination: RegisterOperand::C, source: RegisterOperand::E, ..ld_r_r },
+                    0x4C => Load { destination: RegisterOperand::C, source: RegisterOperand::H, ..ld_r_r },
+                    0x4D => Load { destination: RegisterOperand::C, source: RegisterOperand::L, ..ld_r_r },
 
-                    0x50 => Load { left_operand: RegisterOperand::D, right_operand: RegisterOperand::B, ..ld_r_r },
-                    0x51 => Load { left_operand: RegisterOperand::D, right_operand: RegisterOperand::C, ..ld_r_r },
-                    0x52 => Load { left_operand: RegisterOperand::D, right_operand: RegisterOperand::D, ..ld_r_r },
-                    0x53 => Load { left_operand: RegisterOperand::D, right_operand: RegisterOperand::E, ..ld_r_r },
-                    0x54 => Load { left_operand: RegisterOperand::D, right_operand: RegisterOperand::H, ..ld_r_r },
-                    0x55 => Load { left_operand: RegisterOperand::D, right_operand: RegisterOperand::L, ..ld_r_r },
+                    0x50 => Load { destination: RegisterOperand::D, source: RegisterOperand::B, ..ld_r_r },
+                    0x51 => Load { destination: RegisterOperand::D, source: RegisterOperand::C, ..ld_r_r },
+                    0x52 => Load { destination: RegisterOperand::D, source: RegisterOperand::D, ..ld_r_r },
+                    0x53 => Load { destination: RegisterOperand::D, source: RegisterOperand::E, ..ld_r_r },
+                    0x54 => Load { destination: RegisterOperand::D, source: RegisterOperand::H, ..ld_r_r },
+                    0x55 => Load { destination: RegisterOperand::D, source: RegisterOperand::L, ..ld_r_r },
 
-                    0x58 => Load { left_operand: RegisterOperand::E, right_operand: RegisterOperand::B, ..ld_r_r },
-                    0x59 => Load { left_operand: RegisterOperand::E, right_operand: RegisterOperand::C, ..ld_r_r },
-                    0x5A => Load { left_operand: RegisterOperand::E, right_operand: RegisterOperand::D, ..ld_r_r },
-                    0x5B => Load { left_operand: RegisterOperand::E, right_operand: RegisterOperand::E, ..ld_r_r },
-                    0x5C => Load { left_operand: RegisterOperand::E, right_operand: RegisterOperand::H, ..ld_r_r },
-                    0x5D => Load { left_operand: RegisterOperand::E, right_operand: RegisterOperand::L, ..ld_r_r },
+                    0x58 => Load { destination: RegisterOperand::E, source: RegisterOperand::B, ..ld_r_r },
+                    0x59 => Load { destination: RegisterOperand::E, source: RegisterOperand::C, ..ld_r_r },
+                    0x5A => Load { destination: RegisterOperand::E, source: RegisterOperand::D, ..ld_r_r },
+                    0x5B => Load { destination: RegisterOperand::E, source: RegisterOperand::E, ..ld_r_r },
+                    0x5C => Load { destination: RegisterOperand::E, source: RegisterOperand::H, ..ld_r_r },
+                    0x5D => Load { destination: RegisterOperand::E, source: RegisterOperand::L, ..ld_r_r },
 
-                    0x60 => Load { left_operand: RegisterOperand::H, right_operand: RegisterOperand::B, ..ld_r_r },
-                    0x61 => Load { left_operand: RegisterOperand::H, right_operand: RegisterOperand::C, ..ld_r_r },
-                    0x62 => Load { left_operand: RegisterOperand::H, right_operand: RegisterOperand::D, ..ld_r_r },
-                    0x63 => Load { left_operand: RegisterOperand::H, right_operand: RegisterOperand::E, ..ld_r_r },
-                    0x64 => Load { left_operand: RegisterOperand::H, right_operand: RegisterOperand::H, ..ld_r_r },
-                    0x65 => Load { left_operand: RegisterOperand::H, right_operand: RegisterOperand::L, ..ld_r_r },
+                    0x60 => Load { destination: RegisterOperand::H, source: RegisterOperand::B, ..ld_r_r },
+                    0x61 => Load { destination: RegisterOperand::H, source: RegisterOperand::C, ..ld_r_r },
+                    0x62 => Load { destination: RegisterOperand::H, source: RegisterOperand::D, ..ld_r_r },
+                    0x63 => Load { destination: RegisterOperand::H, source: RegisterOperand::E, ..ld_r_r },
+                    0x64 => Load { destination: RegisterOperand::H, source: RegisterOperand::H, ..ld_r_r },
+                    0x65 => Load { destination: RegisterOperand::H, source: RegisterOperand::L, ..ld_r_r },
 
-                    0x68 => Load { left_operand: RegisterOperand::L, right_operand: RegisterOperand::B, ..ld_r_r },
-                    0x69 => Load { left_operand: RegisterOperand::L, right_operand: RegisterOperand::C, ..ld_r_r },
-                    0x6A => Load { left_operand: RegisterOperand::L, right_operand: RegisterOperand::D, ..ld_r_r },
-                    0x6B => Load { left_operand: RegisterOperand::L, right_operand: RegisterOperand::E, ..ld_r_r },
-                    0x6C => Load { left_operand: RegisterOperand::L, right_operand: RegisterOperand::H, ..ld_r_r },
-                    0x6D => Load { left_operand: RegisterOperand::L, right_operand: RegisterOperand::L, ..ld_r_r },
+                    0x68 => Load { destination: RegisterOperand::L, source: RegisterOperand::B, ..ld_r_r },
+                    0x69 => Load { destination: RegisterOperand::L, source: RegisterOperand::C, ..ld_r_r },
+                    0x6A => Load { destination: RegisterOperand::L, source: RegisterOperand::D, ..ld_r_r },
+                    0x6B => Load { destination: RegisterOperand::L, source: RegisterOperand::E, ..ld_r_r },
+                    0x6C => Load { destination: RegisterOperand::L, source: RegisterOperand::H, ..ld_r_r },
+                    0x6D => Load { destination: RegisterOperand::L, source: RegisterOperand::L, ..ld_r_r },
 
                     _ => panic!(format!("unhandled opcode : 0x{:02X}", word))
                 }
@@ -295,6 +340,10 @@ impl ArrayBasedMemory {
         for i in 0..p.content.len() {
             self.words[i] = p.content[i];
         }
+    }
+
+    fn set_word_at(&mut self, address: Address, word: Word) {
+        self.words[address as usize] = word;
     }
 }
 
@@ -391,6 +440,10 @@ impl ComputerUnit {
 
     fn word_at(&self, address: Address) -> Word {
         self.memory.word_at(address)
+    }
+
+    fn set_word_at(&mut self, address: Address, word: Word) {
+        self.memory.set_word_at(address, word);
     }
 }
 
@@ -564,13 +617,32 @@ fn should_implement_ld_c_prt_hl_instructions() {
     cpu.load(&pg);
     cpu.registers.hl = 0xABCD;
     cpu.memory.words[0xABCD] = 0xEF;
-
-
     assert_eq! (cpu.cycles, 160, "bad cycles count after {}", msg);
 
     cpu.run_1_instruction();
     assert_eq! (cpu.get_c_register(), 0xEF, "bad register value after {}", msg);
     assert_eq! (cpu.get_pc_register(), 0x01, "bad pc after {}", msg);
     assert_eq! (cpu.cycles, 168, "bad cycles count after {}", msg);
+}
 
+#[test]
+fn should_implement_ld_prt_hl_d_instruction() {
+    let pg = Program {
+        name: "LD (HL),D",
+        content: vec![0x72]
+    };
+
+    let msg = pg.name;
+    let mut cpu = new_cpu();
+    cpu.load(&pg);
+    cpu.registers.hl = 0xABCD;
+    cpu.registers.de = 0xEF00;
+    cpu.memory.words[0xABCD] = 0xEF;
+
+    assert_eq! (cpu.cycles, 160, "bad cycles count after {}", msg);
+
+    cpu.run_1_instruction();
+    assert_eq! (cpu.word_at(cpu.get_hl_register()), 0xEF, "bad memory value after {}", msg);
+    assert_eq! (cpu.get_pc_register(), 0x01, "bad pc after {}", msg);
+    assert_eq! (cpu.cycles, 168, "bad cycles count after {}", msg);
 }
