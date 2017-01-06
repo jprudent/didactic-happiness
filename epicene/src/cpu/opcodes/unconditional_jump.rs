7ff4593de455cd2952bb0bb950c32a51ede24f3d
Jérôme Prudent <jprudent@gmail.com>
2017-01-06 02:17:46 +0100

    [EPICENE] Implement JP cond,nn

--- epicene/src/cpu/opcodes/unconditional_jump.rs
+++ epicene/src/cpu/opcodes/unconditional_jump.rs
@@ -1,43 +1,76 @@
 use super::super::{Double, Size, Cycle, Opcode, ComputerUnit};
 use super::super::operands::{RightOperand, ImmediateDouble, DoubleRegister};
+use super::JmpCondition;
 use std::marker::PhantomData;
 
-struct UnconditionalJump<X, A: RightOperand<X>> {
+struct AbsoluteJump<X, A: RightOperand<X>> {
     address: A,
     size: Size,
-    cycles: Cycle,
+    condition: JmpCondition,
+    cycles_when_taken: Cycle,
+    cycles_when_not_taken: Cycle,
     operation_type: PhantomData<X>
 }
 
 pub fn jp_hl() -> Box<Opcode> {
-    Box::new(UnconditionalJump {
+    Box::new(AbsoluteJump {
         address: DoubleRegister::HL,
+        condition:JmpCondition::ALWAYS,
         size: 1,
-        cycles: 4,
+        cycles_when_taken: 4,
+        cycles_when_not_taken: 4,
         operation_type: PhantomData
     })
 }
 
 pub fn jp_nn() -> Box<Opcode> {
-    Box::new(UnconditionalJump {
-        address: ImmediateDouble {},
-        size: 3,
-        cycles: 16,
-        operation_type: PhantomData
-    })
+    jp_cond_nn(JmpCondition::ALWAYS)
 }
 
-impl<A: RightOperand<Double>> Opcode for UnconditionalJump<Double, A> {
+pub fn jp_nz_nn() -> Box<Opcode> {
+    jp_cond_nn(JmpCondition::NONZERO)
+}
+
+pub fn jp_nc_nn() -> Box<Opcode> {
+    jp_cond_nn(JmpCondition::NOCARRY)
+}
+
+pub fn jp_z_nn() -> Box<Opcode> {
+    jp_cond_nn(JmpCondition::ZERO)
+}
+
+pub fn jp_c_nn() -> Box<Opcode> {
+    jp_cond_nn(JmpCondition::CARRY)
+}
+
+fn jp_cond_nn(condition: JmpCondition) -> Box<Opcode> {
+    Box::new(AbsoluteJump {
+           address: ImmediateDouble {},
+           condition: condition,
+           size: 3,
+           cycles_when_taken: 16,
+           cycles_when_not_taken: 12,
+           operation_type: PhantomData
+       })
+}
+
+impl<A: RightOperand<Double>> Opcode for AbsoluteJump<Double, A> {
     fn exec(&self, cpu: &mut ComputerUnit) {
-        let address: Double = self.address.resolve(cpu);
-        cpu.set_register_pc(address - self.size()); // self.size() is added afterward
+        if self.condition.matches(cpu) {
+            let address: Double = self.address.resolve(cpu);
+            cpu.set_register_pc(address - self.size()); // self.size() is added afterward
+        }
     }
 
     fn size(&self) -> Size {
         self.size
     }
 
-    fn cycles(&self, _: &ComputerUnit) -> Cycle {
-        self.cycles
+    fn cycles(&self, cpu: &ComputerUnit) -> Cycle {
+        if self.condition.matches(cpu) {
+            self.cycles_when_taken
+        } else {
+            self.cycles_when_not_taken
+        }
     }
 }
