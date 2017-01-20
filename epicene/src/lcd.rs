a80758af6f23c9b1b6078cfced089fc6fbf0e3ad
Jérôme Prudent <jprudent@gmail.com>
2017-01-20 19:08:48 +0100

    [EPICENE] Implement memory backend for scroll y and monochrome palette

--- epicene/src/lcd.rs
+++ epicene/src/lcd.rs
@@ -15,11 +15,25 @@ impl ColorPalette {
     }
 }
 
+struct MonochromePalette {
+    palette_data: MutableWord
+}
+
+impl MonochromePalette {
+    fn new() -> MonochromePalette {
+        MonochromePalette {
+            palette_data: MutableWord::new(0),
+        }
+    }
+}
+
 pub struct Lcd {
     control: MutableWord,
     y_coordinate: MutableWord,
     x_scroll: MutableWord,
+    y_scroll: MutableWord,
     gbc_color_palette: ColorPalette,
+    gb_monochrome_palette: MonochromePalette,
     gbc_ram_bank_selector: MutableWord,
 }
 
@@ -29,7 +43,9 @@ impl Lcd {
             control: MutableWord::new(0x91), // initial value described in GBCPUman.pdf
             y_coordinate: MutableWord::new(0),
             x_scroll: MutableWord::new(0),
+            y_scroll: MutableWord::new(0),
             gbc_color_palette: ColorPalette::new(),
+            gb_monochrome_palette: MonochromePalette::new(),
             gbc_ram_bank_selector: MutableWord::new(0)
         }
     }
@@ -39,8 +55,10 @@ impl MemoryBacked for Lcd {
     fn word_at(&self, address: Address) -> Word {
         match address {
             0xFF40 => &self.control,
+            0xFF42 => &self.y_scroll,
             0xFF43 => &self.x_scroll,
             0xFF44 => &self.y_coordinate,
+            0xFF47 => &self.gb_monochrome_palette.palette_data,
             0xFF4F => &self.gbc_ram_bank_selector,
             0xFF68 => &self.gbc_color_palette.background_index,
             0xFF69 => &self.gbc_color_palette.background_data,
@@ -51,8 +69,10 @@ impl MemoryBacked for Lcd {
     fn set_word_at(&self, address: Address, word: Word) {
         match address {
             0xFF40 => self.control.set(word),
+            0xFF42 => self.y_scroll.set(word),
             0xFF43 => self.x_scroll.set(word),
             0xFF44 => self.y_coordinate.set(0),
+            0xFF47 => self.gb_monochrome_palette.palette_data.set(word),
             0xFF4F => self.gbc_ram_bank_selector.set(word),
             0xFF68 => self.gbc_color_palette.background_index.set(word),
             0xFF69 => self.gbc_color_palette.background_data.set(word),
