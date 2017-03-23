8a1e2def08b5e648276d174207234ec48b1681e1
Jérôme Prudent <jprudent@gmail.com>
2017-03-23 21:27:03 +0100

    ld *

--- repicene/src/repicene/decoder.clj
+++ repicene/src/repicene/decoder.clj
@@ -108,7 +108,7 @@
   (-> (set-word-at cpu address (low-word val))
       (set-word-at (inc address) (high-word val))))
 
-  (defn dword
+(defn dword
   ([{:keys [memory] :as cpu}]
    {:pre [(not (nil? cpu)) (not (nil? memory))]}
    (cat8 (word-at memory (+ 2 (pc cpu)))
@@ -125,6 +125,13 @@
    (word-at memory (+ 0xFF00 (word cpu))))
   ([cpu val]
    (set-word-at cpu (+ 0xFF00 (word cpu)) val)))
+
+
+(defn <hl>
+  ([{:keys [memory] :as cpu}]
+   (word-at memory (hl cpu)))
+  ([cpu val]
+   (set-word-at cpu (hl cpu) val)))
 
 ;; synonyms to make the code more friendly
 (def <address> dword)
@@ -153,6 +160,69 @@
    0x2E [:ld l word 8 2 #(str "ld l," (hex-word %))]
    0x31 [:ld sp dword 12 3 #(str "ld sp," (hex-dword %))]
    0x3E [:ld a word 8 2 #(str "ld a," (hex-word %))]
+   0x40 [:ld b b 4 1 (constantly "ld b,b")]
+   0x41 [:ld b c 4 1 (constantly "ld b,c")]
+   0x42 [:ld b d 4 1 (constantly "ld b,d")]
+   0x43 [:ld b e 4 1 (constantly "ld b,e")]
+   0x44 [:ld b h 4 1 (constantly "ld b,h")]
+   0x45 [:ld b l 4 1 (constantly "ld b,l")]
+   0x46 [:ld b <hl> 4 1 (constantly "ld b,<hl>")]
+   0x47 [:ld b a 4 1 (constantly "ld b,a")]
+   0x48 [:ld c b 4 1 (constantly "ld c,b")]
+   0x49 [:ld c c 4 1 (constantly "ld c,c")]
+   0x4A [:ld c d 4 1 (constantly "ld c,d")]
+   0x4B [:ld c e 4 1 (constantly "ld c,e")]
+   0x4C [:ld c h 4 1 (constantly "ld c,h")]
+   0x4D [:ld c l 4 1 (constantly "ld c,l")]
+   0x4E [:ld c <hl> 4 1 (constantly "ld c,<hl>")]
+   0x4F [:ld c a 4 1 (constantly "ld c,a")]
+   0x50 [:ld d b 4 1 (constantly "ld d,b")]
+   0x51 [:ld d c 4 1 (constantly "ld d,c")]
+   0x52 [:ld d d 4 1 (constantly "ld d,d")]
+   0x53 [:ld d e 4 1 (constantly "ld d,e")]
+   0x54 [:ld d h 4 1 (constantly "ld d,h")]
+   0x55 [:ld d l 4 1 (constantly "ld d,l")]
+   0x56 [:ld d <hl> 4 1 (constantly "ld d,[hl]")]
+   0x57 [:ld d a 4 1 (constantly "ld d,a")]
+   0x58 [:ld e b 4 1 (constantly "ld e,b")]
+   0x59 [:ld e c 4 1 (constantly "ld e,c")]
+   0x5A [:ld e d 4 1 (constantly "ld e,d")]
+   0x5B [:ld e e 4 1 (constantly "ld e,e")]
+   0x5C [:ld e h 4 1 (constantly "ld e,h")]
+   0x5D [:ld e l 4 1 (constantly "ld e,l")]
+   0x5E [:ld e <hl> 4 1 (constantly "ld e,<hl>")]
+   0x5F [:ld e a 4 1 (constantly "ld e,a")]
+   0x60 [:ld h b 4 1 (constantly "ld h,b")]
+   0x61 [:ld h c 4 1 (constantly "ld h,c")]
+   0x62 [:ld h d 4 1 (constantly "ld h,d")]
+   0x63 [:ld h e 4 1 (constantly "ld h,e")]
+   0x64 [:ld h h 4 1 (constantly "ld h,h")]
+   0x65 [:ld h l 4 1 (constantly "ld h,l")]
+   0x66 [:ld h <hl> 4 1 (constantly "ld h,[hl]")]
+   0x67 [:ld h a 4 1 (constantly "ld h,a")]
+   0x68 [:ld l b 4 1 (constantly "ld l,b")]
+   0x69 [:ld l c 4 1 (constantly "ld l,c")]
+   0x6A [:ld l d 4 1 (constantly "ld l,d")]
+   0x6B [:ld l e 4 1 (constantly "ld l,e")]
+   0x6C [:ld l h 4 1 (constantly "ld l,h")]
+   0x6D [:ld l l 4 1 (constantly "ld l,l")]
+   0x6E [:ld l <hl> 4 1 (constantly "ld l,<hl>")]
+   0x6F [:ld l a 4 1 (constantly "ld l,a")]
+   0x70 [:ld <hl> b 4 1 (constantly "ld [hl],b")]
+   0x71 [:ld <hl> c 4 1 (constantly "ld [hl],c")]
+   0x72 [:ld <hl> d 4 1 (constantly "ld [hl],d")]
+   0x73 [:ld <hl> e 4 1 (constantly "ld [hl],e")]
+   0x74 [:ld <hl> h 4 1 (constantly "ld [hl],h")]
+   0x75 [:ld <hl> l 4 1 (constantly "ld [hl],l")]
+   0x77 [:ld <hl> a 4 1 (constantly "ld [hl],a")]
+   0x78 [:ld a b 4 1 (constantly "ld a,b")]
+   0x79 [:ld a c 4 1 (constantly "ld a,c")]
+   0x7A [:ld a d 4 1 (constantly "ld a,d")]
+   0x7B [:ld a e 4 1 (constantly "ld a,e")]
+   0x7C [:ld a h 4 1 (constantly "ld a,h")]
+   0x7D [:ld a l 4 1 (constantly "ld a,l")]
+   0x7E [:ld a <hl> 4 1 (constantly "ld a,<hl>")]
+   0x7F [:ld a a 4 1 (constantly "ld a,a")]
    0xC0 [:ret-cond :nz address [20 8] 3 #(str "ret nz " (hex-dword %))]
    0xC2 [:jp nz address [16 12] 3 #(str "jp nz " (hex-dword %))]
    0xC3 [:jp always address 8 3 #(str "jp " (hex-dword %))]
